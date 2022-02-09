615e8d3e0657f2b42c9c2332b4dccca8903993d5
kimminkyu <kmk50009@naver.com>
2022-02-09 21:23:30 +0900

    Use MongoDB

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+// /api/new-meetup
+// POST /api/new-meetup
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://elliot:Exjp39Iu2PMgchsB@cluster0.9wywj.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message: 'Meetup inserted!'});
+  }
+}
+
+export default handler;
